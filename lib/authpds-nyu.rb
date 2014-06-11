4aceac93305ec9b5af18a77eb004d6a3ced693e0
Scot Dalton <scotdalton@gmail.com>
2014-06-11 17:03:30 -0400

    Remove exlibris-aleph requirement since it's no longer a dependency

--- lib/authpds-nyu.rb
+++ lib/authpds-nyu.rb
@@ -1,5 +1,4 @@
 require 'authpds'
-require 'exlibris-aleph'
 require 'require_all'
 require_all "#{File.dirname(__FILE__)}/authpds-nyu/"
 Authlogic::Session::Base.send(:include, AuthpdsNyu::Session)
